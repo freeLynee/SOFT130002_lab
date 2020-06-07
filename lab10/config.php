7b068c279f525bf35474953f25074dbc32a3bed3
LIN-YI <1195190035@qq.com>
2020-06-07 23:23:54 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'Tammy_Morris');
+define('DBPASS', '986532145LYly');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
