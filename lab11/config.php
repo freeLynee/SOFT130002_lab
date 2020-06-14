98f8d2f19c38d7be1171168783c700555cd78b43
LIN-YI <1195190035@qq.com>
2020-06-14 21:07:29 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'Tammy_Morris');
+define('DBPASS', '986532145LYly');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login');
+?>
